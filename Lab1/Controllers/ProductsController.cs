bb3dbe0d06279165f2b9ed5f2238832368ab5d4b
jagjeet0824 <jagjeet0824@gmail.com>
2018-02-12 20:52:46 -0500

    Lab1

---
+++ Lab1/Controllers/ProductsController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Lab1.Controllers
+{
+    public class ProductsController : Controller
+    {
+        // GET: Products
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: Store/Details
+        public ActionResult Details(string Products)
+        {
+            ViewBag.Products = Products;
+            return View();
+        }
+
+    }
+}
